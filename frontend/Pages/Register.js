52fb1fe8787f34ed432e33b0d99cdb2156a34545
Cole Hoffman <colehoffman09@gmail.com>
2024-03-14 13:33:31 -0500

    Register Page template

---
+++ frontend/Pages/Register.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+import CustomInput from '../Components/CustomInput';
+import ButtonComponent from '../Components/ButtonComponent';
+
+const colorDarkPurple = '#23027D'; //
+const colorRoyalPurple = '#6E19FF'; // 
+const colorLightPurple = '#D8CCFF'; //
+const colorWhite = '#FFFFFF'; // 
+const colorGray = '#707070'; // 
+const colorBlack = '#FFFFFF'; //
+
+const RegisterPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    return (
+        <View style={{
+            backgroundColor: colorLightPurple,
+        }}>
+            <Text style={{
+                fontFamily: 'Cochin',
+                fontSize: 20,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                paddingBottom: 10,
+                color: colorDarkPurple
+            }}>Create Account</Text>
+            <CustomInput placeholder={"Enter email"}/>
+            <CustomInput placeholder={"Enter password"}/>
+            <CustomInput placeholder={"Confirm password"}/>
+            <ButtonComponent/>
+        </View>
+    );
+
+}
+
+export default RegisterPage;
