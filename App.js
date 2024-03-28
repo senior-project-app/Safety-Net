b901e614b64e6e838c1b175bfb05d5c8936c1f6d
Shelby Hiens <sbhiens@gmail.com>
2024-03-28 13:46:41 -0500

    App.js cleanup

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CustomInput from './frontend/Components/CustomInput';
-import Register from './frontend/Pages/Register';
+import RegisterPage from './frontend/Pages/Register';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register/>
+      <RegisterPage/>
     </View>
   );
 }
