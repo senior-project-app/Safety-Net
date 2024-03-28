7561db34725073e90a92c30ec30fde082df3bb06
Cole Hoffman <colehoffman09@gmail.com>
2024-03-28 11:28:10 -0500

    latest

--- backend/Database.js
+++
@@ -1,6 +0,0 @@
-const Supervisor = require('./Supervisor');
-
-Supervisor.register("Cole Hoffman", "example@example.com", "password_example")
-    .then((res) => {
-        console.log(res);
-    });
