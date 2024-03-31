826cddf29df56c66065eb90d98b81c74835ec189
andromdaa <colehoffman09@gmail.com>
2024-03-31 18:55:34 -0500

    Add fonts via expo-google-fonts, move styles to App.js

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartPage from './frontend/Pages/StartPage';
-import { LogBox } from "react-native";
-LogBox.ignoreLogs(["fontFamily \"Cochin\" is not a system font and has not been loaded through expo-font."]);
+import { useFonts, Inter_900Black, Inter_500Medium } from '@expo-google-fonts/inter';
 
 export default function App() {
+    let [fontsLoaded, fontError] = useFonts({
+        Inter_900Black,
+        Inter_500Medium
+    });
+
+    if (!fontsLoaded) { return null; }
+
     return (
-        <View style={styles.container}>
+        <View style={styles.appContainer}>
             <StartPage />
         </View>
     );
 }
 
+const colorDarkPurple = '#23027D'; //
+const colorRoyalPurple = '#6E19FF'; //
+const colorWhite = '#FFFFFF'; //
+const colorGray = '#707070'; //
+const colorBlack = '#FFFFFF'; //
 const colorLightPurple = '#D8CCFF'; //
-const styles = StyleSheet.create({
-    container: {
+
+export const styles = StyleSheet.create({
+    appContainer: {
         flex: 1,
         backgroundColor: colorLightPurple,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "space-evenly", // for spacing on the vertical
+        alignItems: "center",
+        marginTop: '25%', // forces content to be closer to the center by reducing amount of space for "space-evenly"
+        marginBottom: '25%', // forces conent to be closer to the center
+        backgroundColor: colorLightPurple,
+    },
+    child: {
+        width: "75%" // makes children be the same width
+    },
+    registerButton: {
+        fontFamily: 'Inter_500Medium',
+        fontWeight: 'regular',
+        color: "blue",
+    },
+    text: {
+        alignSelf: "flex-start",
+        fontFamily: 'Inter_500Medium',
+        fontSize: 20,
+        fontWeight: 'regular',
+        color: colorDarkPurple,
+    },
+    logo: {
+        fontFamily: 'Inter_900Black',
+        fontSize: 40,
+        fontWeight: 'regular',
+        color: colorDarkPurple,
     },
 });
